acfd1d4727cea7a7b98698cfa6ba680511d26b43
Jeff.A <kinesthesiac@gmail.com>
2021-11-14 01:15:01 -0800

    basic logic sketch

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -1,3 +1,36 @@
-$('#testBtn').click(function() {
-    console.log("clicked")
+const sorry = "Not quite... Try again! :*"
+let step = 0
+let prompt = [
+    "The place with the bad cat food.",
+    "The best place to hide weed or filing cabinets.",
+    "The place with the garden tools.",
+    "In the linnen closet between these objects."
+]
+let correctAnswer = [
+    "cabinet",
+    "plant lab",
+    "shed",
+    "blanket"
+]
+
+
+$(document).ready(function(e) {
+    $('#hint').text(prompt[step])
+    // e.preventDefault();
+})
+
+$(function() {
+    $('#answer').submit(function(e) {
+        let userAnswer = $('#answerText').val()
+        console.log(userAnswer)
+
+        if (userAnswer == correctAnswer[step]) {
+            $('#feedback').text("You're right!!! Hurray!!!")
+            step += 1
+            $('#hint').text(prompt[step])
+        } else {
+            $('#feedback').text(sorry)
+        }
+        e.preventDefault();
+    })
 })
